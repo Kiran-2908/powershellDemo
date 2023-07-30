0cc658a44d53706ac16cd2830a5df1cbad07bf7b
Kiran-2908 <122803891+Kiran-2908@users.noreply.github.com>
2023-07-30 23:14:43 +0530

    Removed changs

--- DownloadPython.ps1
+++ DownloadPython.ps1
@@ -1,8 +1,1 @@
-﻿function DownloadPythonExecutable()
-{
-    $process = Start-Process -FilePath "$PSScriptRoot\python-3.10.0-amd64.exe" -ArgumentList "/quiet /install" -Wait -PassThru -NoNewWindow
-    Write-Host $process.ExitCode
-}
 
-
-DownloadPythonExecutable
